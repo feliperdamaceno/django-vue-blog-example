43be3b516c962994ee91e4b908932fdb2e58551a
feliperdamaceno <feliperdamaceno@sent.com>
2023-12-25 18:50:55 +0000

    feat: Implement blog posts.

---
+++ frontend/src/components/PostCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Post } from '../@types'
+defineProps<{ post: Post }>()
+</script>
+
+<template>
+  <div class="max-w-xl overflow-hidden bg-white rounded-md shadow-md">
+    <img class="object-cover w-full max-h-60" :src="post.thumbnail" alt="" />
+    <div class="p-6 space-y-2">
+      <h2 class="text-xl font-medium">{{ post.title }}</h2>
+      <p>{{ post.body.substring(0, 125) }}...</p>
+    </div>
+  </div>
+</template>
